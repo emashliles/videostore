c43678250f7c7d0d205a179dfd821eec3e920f7b
EA <emashliles@gmail.com>
2017-01-19 10:40:23 +0000

    Formats the assertion statement to be more readable.

--- java/videostore/src/VideoStoreTest.java
+++ java/videostore/src/VideoStoreTest.java
@@ -12,18 +12,31 @@ public class VideoStoreTest extends TestCase
 
 	public void testSingleNewReleaseStatement () {
 		customer.addRental (new Rental (new Movie ("New Release", Movie.NEW_RELEASE), 3));
-		assertEquals ("Rental Record for Customer\n\tNew Release\t9.0\nYou owed 9.0\nYou earned 2 frequent renter points\n", customer.statement ());
+		assertEquals ("Rental Record for Customer\n" +
+				"\tNew Release\t9.0\n" +
+				"You owed 9.0\n" +
+				"You earned 2 frequent renter points\n",
+				customer.statement ());
 	}
 
 	public void testDualNewReleaseStatement () {
 		customer.addRental (new Rental (new Movie ("New Release", Movie.NEW_RELEASE), 3));
 		customer.addRental (new Rental (new Movie ("Another New Release", Movie.NEW_RELEASE), 3));
-		assertEquals ("Rental Record for Customer\n\tNew Release\t9.0\n\tAnother New Release\t9.0\nYou owed 18.0\nYou earned 4 frequent renter points\n", customer.statement ());
+		assertEquals ("Rental Record for Customer\n" +
+				"\tNew Release\t9.0\n" +
+				"\tAnother New Release\t9.0\n" +
+				"You owed 18.0\n" +
+				"You earned 4 frequent renter points\n",
+				customer.statement ());
 	}
 
 	public void testSingleChildrensStatement () {
 		customer.addRental (new Rental (new Movie ("Childrens", Movie.CHILDRENS), 3));
-		assertEquals ("Rental Record for Customer\n\tChildrens\t1.5\nYou owed 1.5\nYou earned 1 frequent renter points\n", customer.statement ());
+		assertEquals ("Rental Record for Customer\n" +
+				"\tChildrens\t1.5\n" +
+				"You owed 1.5\n" +
+				"You earned 1 frequent renter points\n",
+				customer.statement ());
 	}
 
 	public void testMultipleRegularStatement () {
@@ -31,7 +44,13 @@ public class VideoStoreTest extends TestCase
 		customer.addRental (new Rental (new Movie ("Another Regular", Movie.REGULAR), 2));
 		customer.addRental (new Rental (new Movie ("Different Regular", Movie.REGULAR), 3));
 
-		assertEquals ("Rental Record for Customer\n\tRegular\t2.0\n\tAnother Regular\t2.0\n\tDifferent Regular\t3.5\nYou owed 7.5\nYou earned 3 frequent renter points\n", customer.statement ());
+		assertEquals ("Rental Record for Customer\n" +
+				"\tRegular\t2.0\n" +
+				"\tAnother Regular\t2.0\n" +
+				"\tDifferent Regular\t3.5\n" +
+				"You owed 7.5\n" +
+				"You earned 3 frequent renter points\n",
+				customer.statement ());
 	}
 
 	private Customer customer;
