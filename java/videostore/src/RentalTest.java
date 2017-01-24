0762874a42fbbf482ed276542c13aced00f89616
EA <emashliles@gmail.com>
2017-01-24 11:22:55 +0000

    Simplify rental cost tests.

--- java/videostore/src/RentalTest.java
+++ java/videostore/src/RentalTest.java
@@ -5,25 +5,41 @@ import static junit.framework.TestCase.assertEquals;
 public class RentalTest {
 
     @Test
-    public void TotalCostForRegularMovieCanCalculateAmountForMoreThan2Days() {
+    public void CalculatesFrequentRenterPoints(){
         Rental rental = new Rental(Movie.createMovie("Regular", MovieTypes.REGULAR), 3);
-        double totalCostForRegualrMovie = rental.getTotalRentalCost();
-        assertEquals(3.5, totalCostForRegualrMovie);
+        int frequentRenterPoints = rental.getFrequentRenterPoints();
+
+        assertEquals(1, frequentRenterPoints);
+    }
+
+    @Test
+    public void CalculatesFrequentRenterPointsForNewRelease(){
+        Rental rental = new Rental(Movie.createMovie("NewRelease", MovieTypes.NEW_RELEASE), 3);
+        int frequentRenterPoints = rental.getFrequentRenterPoints();
+
+        assertEquals(2, frequentRenterPoints);
+    }
+
+    @Test
+    public void TotalCostForRegularMovieCanCalculateAmountForMoreThan2Days() {
+        Movie movie = Movie.createMovie("Regular", MovieTypes.REGULAR);
+        double totalCostForRegularMovie = movie.getTotalRentalCost(3);
+
+        assertEquals(3.5, totalCostForRegularMovie);
     }
 
     @Test
     public void TotalCostForChildrensMovieCanCalculateAmountForMoreThan3Days() {
-        Rental rental = new Rental(Movie.createMovie("Childrens", MovieTypes.CHILDRENS), 4);
-        double totalCostForChildrensMovie = rental.getTotalRentalCost();
+        Movie movie = Movie.createMovie("Childrens", MovieTypes.CHILDRENS);
+        double totalCostForChildrensMovie = movie.getTotalRentalCost(4);
 
         assertEquals(3.0, totalCostForChildrensMovie);
-
     }
 
     @Test
     public void TotalCostForNewReleaseMovieCanCalculateAmount() {
-        Rental rental = new Rental(Movie.createMovie("New Release", MovieTypes.NEW_RELEASE), 3);
-        double totalCostForNewReleaseMovie = rental.getTotalRentalCost();
+        Movie movie = Movie.createMovie("New Release", MovieTypes.NEW_RELEASE);
+        double totalCostForNewReleaseMovie = movie.getTotalRentalCost(3);
 
         assertEquals(9.0, totalCostForNewReleaseMovie);
 
