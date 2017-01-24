d7e54e98b977d07700363ce658745aa4a123613e
EA <emashliles@gmail.com>
2017-01-24 10:58:02 +0000

    Introduce constants for rental calculations.

--- java/videostore/src/ChildrensMovie.java
+++ java/videostore/src/ChildrensMovie.java
@@ -1,17 +1,22 @@
 public class ChildrensMovie extends Movie {
+
+    public static final double COST_PER_EXTRA_RENTAL_DAY = 1.5;
+    public static final int BASE_RENTAL_DAYS = 3;
+    public static final double BASE_RENTAL_COST = 1.5;
+
     public ChildrensMovie(String title) {
         super(title);
     }
 
     @Override
     public double getTotalRentalCost(int daysRented) {
-        double costPerExtraRentalDay= 1.5;
-        double baseRentalCost= 1.5;
-        int baseRentalDays = 3;
+        double rentalCost = 0.0;
 
-        if (daysRented > baseRentalDays) {
-            baseRentalCost += (daysRented- baseRentalDays) * costPerExtraRentalDay;
+        rentalCost += BASE_RENTAL_COST;
+
+        if (daysRented > BASE_RENTAL_DAYS) {
+            rentalCost += (daysRented- BASE_RENTAL_DAYS) * COST_PER_EXTRA_RENTAL_DAY;
         }
-        return baseRentalCost;
+        return rentalCost;
     }
 }
